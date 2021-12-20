27f3c09fd4dac9b59d416431d1001fcaf81a6c9b
yeji <lerry597@gmail.com>
2021-12-21 01:30:15 +0900

    [feature] retry.js 컴포넌트 / 스타일링

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth';
+import Retry from './pages/Retry';
+import Game from './pages/Game';
+
 import './styles/index.scss';
 
 const App = () => {
@@ -9,6 +12,8 @@ const App = () => {
       <Route path='/' element={<Auth />}></Route>
       <Route path='/challenge'></Route>
       <Route path='/end'></Route>
+      <Route path='/retry' element={<Retry />}></Route>
+      <Route path='/game' element={<Game />}></Route>
     </Routes>
   );
 };
---
+++ src/pages/Game.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { game } from '../api/game';
+
+const Game = () => {
+  return <div className='GameWrapper'></div>;
+};
+
+export default Game;
