1e334a36060155bdce600810e5f10270e0dfed17
zeze Jeong <zeze@linercorp.com>
2021-12-21 03:03:18 +0900

    [feature] 로그인 화면 개발 완료

---
+++ src/components/SignIn.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { login } from '../api/auth';
+
+const SignIn = ({ changeMode }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const changeUsername = (value) => {
+    setUsername(value);
+  };
+
+  const changePassword = (value) => {
+    setPassword(value);
+  };
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const userInfo = {
+      username,
+      password,
+    };
+
+    try {
+      const res = await login(userInfo);
+    } catch (err) {}
+  };
+
+  return (
+    <div>
+      <h1 className='SignIn__header'>Sign In</h1>
+      <form onSubmit={submitForm}>
+        <div className='SignIn__inputBox'>
+          <label className='SignIn__label' htmlFor='SignIn__input--username'>
+            ID
+          </label>
+          <input
+            id='SignIn__input--username'
+            className='SignIn__input'
+            type='text'
+            placeholder='venture@snu.ac.kr'
+            onChange={(e) => changeUsername(e.target.value)}
+          />
+        </div>
+        <div className='SignIn__inputBox'>
+          <label className='SignIn__label' htmlFor='SignIn__input--password'>
+            Password
+          </label>
+          <input
+            id='SignIn__input--password'
+            className='SignIn__input'
+            type='password'
+            placeholder='••••••'
+            onChange={(e) => changePassword(e.target.value)}
+          />
+        </div>
+        <button className='SignIn__btn' type='submit'>
+          Submit
+        </button>
+      </form>
+      <div className='Auth__another'>
+        <span className='Auth__already'>Don't have an account?</span>
+        <button className='Auth__signInBtn' onClick={changeMode}>
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { signup } from '../api/auth';
+import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
 
 const Auth = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
   const [isShowTime, setIsShowTime] = useState(false);
+  const [isSignIn, setIsSignIn] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -13,79 +12,14 @@ const Auth = () => {
     }, 500);
   }, []);
 
-  const changeUsername = (value) => {
-    setUsername(value);
-  };
-
-  const changePassword = (value) => {
-    setPassword(value);
-  };
-
-  const changeName = (value) => {
-    setName(value);
-  };
-
-  const submitForm = async (e) => {
-    e.preventDefault();
-    const userInfo = {
-      username,
-      password,
-      name,
-    };
-
-    try {
-      const res = await signup(userInfo);
-      console.log(res);
-    } catch (err) {
-      console.error(err);
-    }
+  const changeMode = () => {
+    setIsSignIn(!isSignIn);
   };
 
   return (
     <div className='AuthWrapper'>
       <div className={`Auth ${isShowTime ? 'show' : ''}`}>
-        <h1 className='Auth__header'>Welcome</h1>
-        <form onSubmit={submitForm}>
-          <div className='Auth__inputBox'>
-            <label className='Auth__label' htmlFor='Auth__input--username'>
-              Username
-            </label>
-            <input
-              id='Auth__input--username'
-              className='Auth__input'
-              type='text'
-              placeholder='venture@snu.ac.kr'
-              onChange={(e) => changeUsername(e.target.value)}
-            />
-          </div>
-          <div className='Auth__inputBox'>
-            <label className='Auth__label' htmlFor='Auth__input--password'>
-              Password
-            </label>
-            <input
-              id='Auth__input--password'
-              className='Auth__input'
-              type='password'
-              placeholder='••••••'
-              onChange={(e) => changePassword(e.target.value)}
-            />
-          </div>
-          <div className='Auth__inputBox'>
-            <label className='Auth__label' htmlFor='Auth__input--name'>
-              Name
-            </label>
-            <input
-              id='Auth__input--name'
-              className='Auth__input'
-              type='name'
-              placeholder='jin'
-              onChange={(e) => changeName(e.target.value)}
-            />
-          </div>
-          <button className='Auth__btn' type='submit'>
-            Submit
-          </button>
-        </form>
+        {isSignIn ? <SignIn changeMode={changeMode} /> : <SignUp changeMode={changeMode} />}
       </div>
     </div>
   );
