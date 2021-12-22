715e2e05d684a0327523ef9da0268948c582d198
zeze Jeong <zeze@linercorp.com>
2021-12-22 16:09:51 +0900

    [feature] 엔딩 버튼 이슈 해결

--- src/api/battle.js
+++ src/api/battle.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { SERVER_URL } from '../config';
 
-export const getAttackResult = (turn, monsterName) => {
+export const getAttackResult = (turn, monsterName, monsterHp) => {
   const cookie = localStorage.getItem('accessToken');
 
   return axios({
     method: 'get',
     baseURL: SERVER_URL,
-    url: `/battle/${turn}/${monsterName}`,
+    url: `/battle/${turn}/${monsterName}${monsterHp ? `/${monsterHp}` : ''}`,
     headers: {
       Authorization: `Bearer ${cookie}`,
       'Content-Type': 'application/json',
